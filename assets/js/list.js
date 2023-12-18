9174ce1c4d03fbf22aa90954181e4d344f589b25
Alice Marique <mariquealice83@gmail.com>
2023-12-18 11:33:11 +0100

    liste js + img by Alice

---
+++ assets/js/list.js
@@ -0,0 +1,84 @@
+const plats = [
+    {
+        picture: "img/plats/Bruschetta.jpg",
+        categorie: "Entrée",
+        name: "Bruschetta",
+        description: "Pain grillé frotté à l'ail et garni de tomates, basilic et huile d'olive.",
+        price: "8.99 €",
+        area: "Rome",
+        
+    },
+    {
+        picture: "img/plats/salade-capresse.jpg",
+        categorie: "Entrée",
+        name: "Salade Caprese",
+        description: "Salade de tomates, mozzarella et basilic frais.",
+        price: "9.99 €",
+        area: "Ile de Capri",
+        
+    },
+    {
+        picture: "img/plats/Antipasto.jpg",
+        categorie: "Entrée",
+        name: "Plateau Antipasto",
+        description: "Assortiment de fromages, charcuteries, olives et légumes marinés.",
+        price: "12.99 €",
+        area: "Italie",
+        
+    },
+    {
+        picture: "img/plats/carbonara.jpg",
+        categorie: "Plat principal",
+        name: "Spaghetti Carbonara",
+        description: "Pâtes avec une sauce crémeuse, du lard et du fromage pecorino.",
+        price: "14.99 €",
+        area: "Rome",
+        
+    },
+    {
+        picture: "img/plats/Risotto.jpg",
+        categorie: "Plat principal",
+        name: "Risotto Milanese",
+        description: "Riz crémeux avec du safran et du parmesan.",
+        price: "16.99 €",
+        area: "Milan",
+        
+    },
+    {
+        picture: "img/plats/osso-bucco.jpeg",
+        categorie: "Plat principal",
+        name: "Osso Buco",
+        description: "Plat de viande mijotée, généralement de la jarret de veau, servi avec une sauce.",
+        price: "18.99 €",
+        area: "Milan",
+        
+    },
+    {
+        picture: "img/plats/tiramisu.jpg",
+        categorie: "Dessert",
+        name: "Tiramisu",
+        description: "Dessert au café et mascarpone, saupoudré de cacao.",
+        price: "9.99 €",
+        area: "Trévise",
+        
+    },
+    {
+        picture: "img/plats/pana.jpg",
+        categorie: "Dessert",
+        name: "Panna Cotta",
+        description: "Dessert crémeux à la vanille avec coulis de fruits rouges.",
+        price: "8.99 €",
+        area: "Piémont",
+        
+    },
+    {
+        picture: "img/plats/gelato.jpg",
+        categorie: "Dessert",
+        name: "Gelato",
+        description: "Sélection de glaces artisanales italiennes.",
+        price: "7.99 €",
+        area: "Florence",
+        
+    },
+   
+];
