1ba54f8b71e0b99d01b04cb4daa8317303c341dd
huseyinsasmaz2001 <huseyinsasmaz2001@gmail.com>
2023-12-20 13:57:22 +0100

    creation des button + ,-, et Supprimer et c'est button sont fonctionel

--- assets/js/panier.js
+++ assets/js/panier.js
@@ -1,5 +1,52 @@
-// Tableau pour stocker les éléments du panier
 let panierItems = [];
+
+
+
+function augmenterQuantite(index) {
+    if (index >= 0 && index < panierItems.length) {
+        const elementHTML = document.getElementById(`quantite-${index}`); // Obtenez l'élément HTML pour la quantité
+        panierItems[index].quantity += 1;
+        if (elementHTML && !isNaN(panierItems[index].quantity)) {
+            elementHTML.innerText = parseInt(panierItems[index].quantity, 10);
+        }
+        mettreAJourPanierEtTotal();
+    }
+}
+
+function diminuerQuantite(index) {
+    if (index >= 0 && index < panierItems.length && panierItems[index].quantity > 1) {
+        const elementHTML = document.getElementById(`quantite-${index}`);
+        panierItems[index].quantity -= 1;
+
+        if (elementHTML) {
+            elementHTML.innerText = parseInt(panierItems[index].quantity, 10);
+        }
+        mettreAJourPanierEtTotal();
+    }
+}
+function mettreAJourPanierEtTotal() {
+    const elementHTML = document.getElementById('panier');
+    if (!elementHTML) return;
+
+    const totalAvant = calculerTotal();
+
+    panierItems = panierItems.filter(item => item.quantity > 0);
+
+    afficherPanier(elementHTML);
+
+    const nouveauTotal = calculerTotal();
+
+    if (totalAvant !== nouveauTotal) {
+        afficherTotal(nouveauTotal);
+    }
+}
+
+function afficherTotal(total) {
+    const totalElement = document.getElementById('total'); // Assurez-vous d'avoir un élément avec l'ID "total"
+    if (totalElement) {
+        totalElement.innerText = `Total: ${total}`;
+    }
+}
 
 function calculerTotal() {
     let total = 0;
@@ -15,22 +62,66 @@ function calculerTotal() {
 
     return total;
 }
-// Fonction pour ajouter un plat au panier
+
 function ajouterAuPanier(plat) {
-    panierItems.push(plat);
+    const indexExistant = panierItems.findIndex(item => item.name === plat.name);
+
+    if (indexExistant !== -1) {
+        augmenterQuantite(indexExistant);
+    } else {
+        plat.quantity = 1;
+        panierItems.push(plat);
+        mettreAJourPanierEtTotal();
+    }
 }
 
+
+// ... ton code précédent inchangé ...
 function afficherPanier(elementHTML) {
-    // Efface le contenu précédent de l'élément HTML du panier
     elementHTML.innerHTML = '';
 
-    
-    panierItems.forEach((item) => {
+    panierItems.forEach((item, index) => {
         const listItem = document.createElement('li');
-        listItem.innerText = `${item.name} - Prix: ${item.price}`; 
+        listItem.innerText = `${item.name} - Prix: ${item.price} - Quantité:`;
+
+        const quantiteText = document.createElement('span');
+        quantiteText.id = `quantite-${index}`;
+        quantiteText.innerText = ` ${item.quantity}`; // Affichage de la quantité entre les boutons
+
+        const augmenterButton = document.createElement('button');
+        augmenterButton.innerText = '+';
+        augmenterButton.addEventListener('click', () => {
+            augmenterQuantite(index);
+        });
+
+        const diminuerButton = document.createElement('button');
+        diminuerButton.innerText = '-';
+        diminuerButton.addEventListener('click', () => {
+            diminuerQuantite(index);
+        });
+
+        listItem.appendChild(augmenterButton);
+        listItem.appendChild(quantiteText);
+        listItem.appendChild(diminuerButton);
+        const supprimerButton = document.createElement('button');
+        supprimerButton.innerText = 'Supprimer';
+        supprimerButton.addEventListener('click', () => {
+            if (index >= 0 && index < panierItems.length) {
+                panierItems.splice(index, 1);
+                mettreAJourPanierEtTotal();
+            }
+
+            // Actualiser l'affichage du panier après la suppression
+            afficherPanier(elementHTML);
+        });
+
+        listItem.appendChild(supprimerButton);
         elementHTML.appendChild(listItem);
     });
-
 }
 
-export { ajouterAuPanier, afficherPanier, calculerTotal};
+
+
+
+// Exportation des fonctions
+export { ajouterAuPanier, afficherPanier, calculerTotal, afficherTotal };
